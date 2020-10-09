228fa4cc4b19abe959b893b845a43e90e9d40fc3
unknown <guilhermealves2596@gmail.com>
2020-10-08 21:13:14 -0300

    Adding random position cards function
    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# dio-classic-memory-game
-Jogo da memória clássico desenvolvido com Javascript, HTML5 e CSS3
+# Jogo da Memória Clássico em Javascript, HTML5 e CSS3
+
+Jogo da memória clássico desenvolvido com Javascript, HTML5 e CSS3 junto ao Bootcamp de desenvolvimento de jogos com Javascrip para fins de estudos.
+
+### Técnologias utilizadas 
+
+- Javascript
+- HTML5
+- CSS3
+
+Projeto com intenção de explorar alguns recursos do CSS, com objetivo de trazer melhor interatividade e animações para o usuário 
+
+#### Recursos CSS explorados :
+
+- transform
+- rotateY
+- transition
+- perspective
+- transform-style
+- backface-visibility
+
+### Protótipos 
+
+#### Tela inicial 
+
+![Memory Game Example](https://github.com/guilherme25alves/dio-classic-memory-game/blob/main/img/game.png?raw=true)
+
+#### Demonstração do Game 
+
+![Memory Game Demo Example](https://github.com/guilherme25alves/dio-classic-memory-game/blob/main/img/demo-game.png?raw=true)
--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function flipCard() {
 
      if(lockBoard) return;     
 
+     if(this === firstCard) return;
+
      this.classList.add('flip'); // toggle => Efeito de tirar e adicionar || ADD -> Apenas adicionar 1 vez
 
      if(!hasFlippedCard) {
@@ -33,14 +35,40 @@ function checkforMath() {
 function disableCards() {
      firstCard.removeEventListener('click', flipCard);
      secondCard.removeEventListener('click', flipCard);
+
+     resetBoard();
 }
 
 function unflipCards() {
+     lockBoard = true;
+
      setTimeout(() => {
           firstCard.classList.remove('flip');
           secondCard.classList.remove('flip');
+
+          resetBoard();
+          //lockBoard = false;
      }, 1500);
 }
+
+
+/* Desestruturação das nossas variáveis em Arrays */
+function resetBoard() {
+     [hasFlippedCard, lockBoard] = [false, false];
+     [firstCard, secondCard] = [null, null];
+}
+
+
+// Immediately invoked function expression
+     // Quando precisamos declarar uma função e chamar a mesma imediatamente quando nosso código vai rodar 
+          // Usamos essa expressão 
+(function shuffle() {
+     cards.forEach((card) => {
+          let randomPosition = Math.floor(Math.random() * 12);
+          card.style.order = randomPosition;
+     });
+})();
+
 
 cards.forEach((card) => {
      card.addEventListener('click', flipCard);
